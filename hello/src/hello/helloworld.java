fa19404de90647885c266fa4d7d78ca62bdac9ce
iftikhar <iftiali@gmail.com>
2018-06-14 10:50:03 -0400

    Adding getNumber method to main class

---
+++ hello/src/hello/helloworld.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class helloworld {
+	public static void main (String args[]) {
+		System.out.println("Hello Pakistan");
+	}
+// Adding this method to see the affect on local repo
+	public int getNumber() {
+		return 0;
+	}
+}
